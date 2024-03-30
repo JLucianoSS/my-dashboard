1da26fc54cb454bbe191c74ac3f59e10a6315ec4
Jorge Luciano <jorgelss912345@gmail.com>
2024-03-29 20:59:59 -0500

    First commit

---
+++ src/app/dashboard/main/page.tsx
@@ -0,0 +1,10 @@
+
+
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Hello Main Page</h1>
+    </div>
+  );
+}
---
+++ src/app/shopping-cart/index.tsx
@@ -0,0 +1,4 @@
+import CounterPage from '../dashboard/counter/page';
+
+
+export { CartCounter } from './components/CartCounter';
